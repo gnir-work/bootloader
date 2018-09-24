2bc4114b4a7a74ead729c5a8275d3e8c102410eb
nir geller <nir@debian.debian_pc>
2018-09-24 17:05:04 +0300

    Make the mbr print 'hello world', still no idea as to how it works.

--- boot.asm
+++ boot.asm
@@ -1,7 +1,19 @@
-    cli
-hang:
-    jmp hang
+; boot.asm
+   mov ax, 0x07c0
+   mov ds, ax
  
-    times 510-($-$$) db 0 ; 2 bytes less now
-    db 0x55
-    db 0xAA
+   mov si, msg
+ch_loop:lodsb
+   or al, al ; zero=end or str
+   jz hang   ; get out
+   mov ah, 0x0E
+   int 0x10
+   jmp ch_loop
+ 
+hang:
+   jmp hang
+ 
+msg   db 'Hello World', 13, 10, 0
+   times 510-($-$$) db 0
+   db 0x55
+   db 0xAA
